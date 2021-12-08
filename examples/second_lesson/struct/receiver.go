c4b7091e658837c1a05f9c60b9afbfcf63e4930e
Jonye <Jonye@email.com>
2021-12-08 23:19:30 +0800

    [新增代码]接口使用和接收器使用说明

--- examples/second_lesson/struct/receiver.go
+++ examples/second_lesson/struct/receiver.go
@@ -7,7 +7,7 @@ func main() {
 	// 因为 u 是结构体，所以方法调用的时候它数据是不会变的
 	u := User{
 		Name: "Tom",
-		Age: 10,
+		Age:  10,
 	}
 	u.ChangeName("Tom Changed!")
 	u.ChangeAge(100)
@@ -16,7 +16,7 @@ func main() {
 	// 因为 up 指针，所以内部的数据是可以被改变的
 	up := &User{
 		Name: "Jerry",
-		Age: 12,
+		Age:  12,
 	}
 
 	// 因为 ChangeName 的接收器是结构体
@@ -29,15 +29,24 @@ func main() {
 
 type User struct {
 	Name string
-	Age int
+	Age  int
 }
 
 // 结构体接收器
-func (u User) ChangeName(newName string)  {
+// 当我们提供给外的接口不想让别人修改我们结构体数据时，使用结构体接收器，
+// 其实就是值复制，无论其他地方怎么改，都不会影响结构体的数据
+func (u User) ChangeName(newName string) {
 	u.Name = newName
 }
 
-// 指针接收器
+// 指针接收器	遇事不决用指针
 func (u *User) ChangeAge(newAge int) {
 	u.Age = newAge
+}
+
+//type A B类型则使用结构体
+type handle func()
+
+func (h handle) Hello() {
+
 }
--- onclass/main.go
+++ onclass/main.go
@@ -1,4 +1,4 @@
-package main
+package onclass
 
 import (
 	"fmt"
@@ -17,10 +17,9 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "这是创建用户")
 }
 
-func order(w http.ResponseWriter, r *http.Request)  {
+func order(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "这是订单")
 }
-
 
 func main() {
 	http.HandleFunc("/", home)
@@ -37,4 +36,16 @@ type Server interface {
 
 type sdkHttpServer struct {
 	Name string
+}
+
+func (s *sdkHttpServer) Route(pattern string, handlerFunc http.HandlerFunc) {
+	http.HandleFunc(pattern, handlerFunc)
+}
+
+func (s *sdkHttpServer) Start(address string) error {
+	return http.ListenAndServe(address, nil)
+}
+
+func NewHttpServer() Server {
+	return &sdkHttpServer{}
 }
